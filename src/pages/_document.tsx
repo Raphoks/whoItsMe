452f945afd01be316543701498abf3ba998ff951
Lucas Duarte <lucasdu4rte@gmail.com>
2022-08-22 23:38:30 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
 
           <link rel="shortcut icon" href="/favicon.png" type="image/png" />
           
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         </Head>
         <body>
           <Main />
